95f5bf712b038fda8dec570fc822c819a81ab83d
Will Hogan <willhogan11@hotmail.com>
2015-07-23 00:34:13 +0100

    Final Version

---
+++ src/gmit/Person.java
@@ -0,0 +1,14 @@
+package gmit;
+
+public class Person {
+	
+	String name;
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public String getName(){
+		return name;
+	}
+}
---
+++ src/gmit/methodExamples.java
@@ -0,0 +1,34 @@
+package gmit;
+import java.util.Scanner;
+import gmit.Person;
+
+public class methodExamples {
+
+	private static String name;
+	static Scanner input = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+		Person user = new Person();
+		try
+		{
+			System.out.println("Enter Your Name: "); 
+			user.setName(name);
+			String name = input.nextLine();
+			System.out.println(Hello(name));
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	} // End main
+
+	
+	public static String Hello(String name){
+		if(name.equals("Will Hogan")){
+			return "Welcome Will Hogan";
+		}
+		else{
+			return "You are not a Valid User";
+		}
+	}// End Hello method
+}
