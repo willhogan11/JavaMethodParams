3efd26e1d6ab3b9d830d95cba0542f38a8db57a4
Will Hogan <willhogan11@users.noreply.github.com>
2015-07-23 00:38:37 +0100

    Update methodExamples.java

--- src/gmit/methodExamples.java
+++ src/gmit/methodExamples.java
@@ -3,26 +3,22 @@ import java.util.Scanner;
 import gmit.Person;
 
 public class methodExamples {
-
 	private static String name;
 	static Scanner input = new Scanner(System.in);
 	
 	public static void main(String[] args) {
-		
 		Person user = new Person();
-		try
-		{
+		try{
 			System.out.println("Enter Your Name: "); 
 			user.setName(name);
 			String name = input.nextLine();
 			System.out.println(Hello(name));
-		}
-		catch(Exception e){
+		
+		}catch(Exception e){
 			e.printStackTrace();
 		}
 	} // End main
 
-	
 	public static String Hello(String name){
 		if(name.equals("Will Hogan")){
 			return "Welcome Will Hogan";
